64cebbd20cad936d0e8dc7481477c7d201b1507e
Bhoowan <bhoowankapoor.bk@gmail.com>
2023-11-20 14:04:16 +0200

    Submitted week7-lab5

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+
+  // Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div>  <p>Copyright © 2023 My JSX App</p>
+  <p>Design</p></div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+
+  // Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <div>  <h1>My JSX App Header</h1>
+  <p>My JSX App sub Header</p></div>
+    </header>
+  );
+}
+
+export default Header;
